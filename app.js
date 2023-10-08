389dd7568b1c249383c8bfc4eb1c92b982788a7d
itmajster <amlynar9@gmail.com>
2023-10-08 14:08:30 +0200

    ejs to html

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
 
 app.listen(PORT, () => {
  console.log(`Example app listening at http://localhost:${PORT}`);
